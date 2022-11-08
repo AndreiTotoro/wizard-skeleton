f254535b8730bd3b8d9eef34b467a1584eddc735
AndreiTotoro <AndreiTotoro@gmail.com>
2022-11-08 18:28:02 +0200

    Added hover animations

--- components/artwork-creator/components/Preview.js
+++ components/artwork-creator/components/Preview.js
@@ -3,12 +3,12 @@ import React from 'react';
 export default function Preview({ addWizardData, layerData }) {
 	return (
 		<div
-			className="bg-midnight text-white  rounded-lg p-1 w-10/12 my-6 pl-2 flex justify-between"
+			className="bg-midnight text-white  rounded-lg p-1 w-10/12 my-6 pl-2 flex justify-between hover:cursor-pointer"
 			onClick={() => {
 				addWizardData(layerData);
 			}}
 		>
-			<p className="p-2 text-xl">Preview</p>
+			<p className="p-2 text-xl">Export Data</p>
 		</div>
 	);
 }
